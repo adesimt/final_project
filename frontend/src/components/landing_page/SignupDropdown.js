31a4e2d1941858526e5a4b34a4c158fbb43cc158
Adewale <adesimt@dunwoody.edu>
2021-10-19 15:12:24 -0500

    included a drop down to the landing page

--- frontend/src/components/landing_page/SignupDropdown.js
+++ frontend/src/components/landing_page/SignupDropdown.js
@@ -1,23 +1,23 @@
 import { FaTimes } from 'react-icons/fa';
 import './SignupDropdown.css';
 import { Link } from 'react-router-dom';
-// import { useState } from 'react';
+import { useState } from 'react';
 
 
 const SignupDropdown = () => {
 
-    //const [dropdown, setDropdown] = useState(false);
+    const [click, setClick] = useState(false);
 
     return ( 
         <>
-            <div className="dropdown_page">
+            <div className={click ? "dropdown_page clicked" : "dropdown_page"}>
                 <div className="dropdown_content">
-                    <div className="close_icon">
+                    <div className="close_icon" onClick={() => setClick(!click)}>
                         <FaTimes size="2em" color="#1A272A" />
                     </div>
 
                     <div className="signup_items signup_client">
-                        <Link to="/signup-as-a-client" className="signup_client_items">
+                        <Link to="/signup-as-a-client" className="signup_client_items" onClick = {()=> setClick(false)}>
                             <div className="signup_links">
                                 Create a client account
                             </div>
@@ -27,7 +27,7 @@ const SignupDropdown = () => {
 
                     <div className="signup_items signup_trainer">
 
-                        <Link to="/signup-as-a-trainer" className="signup_trainer_items">
+                        <Link to="/signup-as-a-trainer" className="signup_trainer_items" onClick = {()=> setClick(false)}>
                             <div className="signup_links">
                                 Create a trainer account
                             </div>
